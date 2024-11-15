9211d84a9b4330d38d9f051f47d340c3df8fcffe
Zach Leatherman <zachleat@users.noreply.github.com>
2024-11-15 14:52:38 -0600

    Upgrade dep

--- import.sh
+++ import.sh
@@ -5,6 +5,5 @@ else
 	persist=""
 fi
 
-echo $persist
 npx @11ty/import bluesky @zachleat.com --output=bluesky --cacheduration=4h $persist
 npx @11ty/import fediverse zachleat@fediverse.zachleat.com --output=fediverse --cacheduration=4h $persist
